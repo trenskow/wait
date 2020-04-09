3d5fa0b17654c7812cf731094efc4226cfb7a3b4
Kristian Trenskow <trenskow@me.com>
2020-04-09 11:06:40 +0200

    Implemented the ability to await a number (ms) also.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const ms = require('ms');
 
 module.exports = exports = (interval) => {
-	return new Promise((resolv) => {
-		setTimeout(resolv, ms(interval));
+	return new Promise((resolve) => {
+		if (typeof interval === 'string') interval = ms(interval);
+		setTimeout(resolve, interval);
 	});
 };
--- test/index.js
+++ test/index.js
@@ -7,10 +7,18 @@ const { expect } = require('chai');
 const wait = require('../');
 
 describe('wait', () => {
-	it('should wait one seconds.', () => {
+	it('should wait one seconds (string).', () => {
 		const startTime = new Date();
 		return expect(
 			wait('1s').then(() => {
+				return (new Date()).getTime() - startTime.getTime();
+			}))
+			.to.eventually.be.satisfy((val) => val >= 1000);
+	});
+	it('should wait one seconds (number).', () => {
+		const startTime = new Date();
+		return expect(
+			wait(1000).then(() => {
 				return (new Date()).getTime() - startTime.getTime();
 			}))
 			.to.eventually.be.satisfy((val) => val >= 1000);
